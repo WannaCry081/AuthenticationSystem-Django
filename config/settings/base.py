a53c8780b77107d48c88a68a24fa3528e71c062b
Lirae Que Data <liraedata59@gmail.com>
2024-05-26 12:31:21 +0800

    Add USER_AUTH_MODEL constant

--- config/settings/base.py
+++ config/settings/base.py
@@ -110,3 +110,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+USER_AUTH_MODEL = 'v1.User'
