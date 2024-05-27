ab91dec3c213821290654396b13226c47ecd82c1
Lirae Que Data <liraedata59@gmail.com>
2024-05-27 20:40:41 +0800

    Add RegisterSerializer in register_serializer.py

---
+++ api/v1/serializers/register_serializer.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from api.v1.models import User
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+
+    firstName = serializers.CharField(
+                        required = True,
+                        source = "first_name")    
+    lastName = serializers.CharField(
+                        required = True,
+                        source = "last_name")    
+    password = serializers.CharField(
+                        required = True)
+    rePassword = serializers.CharField(
+                        required = True)
+    
+    class Meta:
+        
+        model = User
+        fields = ["id",
+                  "username",
+                  "firstName",
+                  "lastName",
+                  "email",
+                  "password"
+                  "rePassword"]
+        extra_kwargs = {
+            "id" : { "read_only" : True }
+        }
