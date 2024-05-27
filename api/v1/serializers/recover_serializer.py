13d7fb294ac4f88abf5488087dd43875059fb858
Lirae Que Data <liraedata59@gmail.com>
2024-05-27 21:29:34 +0800

    Add ResetPasswordSerializer in recover_serializer.py

--- api/v1/serializers/recover_serializer.py
+++ api/v1/serializers/recover_serializer.py
@@ -5,3 +5,13 @@ class ForgotPasswordSerializer(serializers.Serializer):
     
     email = serializers.EmailField(
                     required = True)
+
+
+class ResetPasswordSerializer(serializers.Serializer):
+    
+    email = serializers.EmailField(
+                    required = True)
+    resetCode = serializers.CharField(
+                    required = True)
+    newPassword = serializers.CharField(    
+                    required = True)
