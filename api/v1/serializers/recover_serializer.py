dcb029d9834aa2d5313b9f3c63c85228a817e42c
Lirae Que Data <liraedata59@gmail.com>
2024-06-04 21:59:51 +0800

    Add validate method in recover_serializer.py

--- api/v1/serializers/recover_serializer.py
+++ api/v1/serializers/recover_serializer.py
@@ -1,10 +1,32 @@
 from rest_framework import serializers
+from rest_framework.exceptions import (ParseError,
+                                       AuthenticationFailed)
+from api.v1.models import User
+from api.v1.utils import CodeGenerator
 
 
 class ForgotPasswordSerializer(serializers.Serializer):
     
     email = serializers.EmailField(
                     required = True)
+
+    def validate(self, attrs):
+        
+        if "email" not in attrs:
+            raise ParseError(
+                detail = "Invalid request. Please try again.")
+            
+        user = User.objects.get(email = attrs.get("email"))
+        code : str = CodeGenerator()
+        if not user:
+            raise AuthenticationFailed(
+                detail = "Invalid credentials. Please try again.")
+        
+        user.reset_code = code
+        user.save()        
+    
+        attrs["user"] = user
+        return attrs
 
 
 class ResetPasswordSerializer(serializers.Serializer):
