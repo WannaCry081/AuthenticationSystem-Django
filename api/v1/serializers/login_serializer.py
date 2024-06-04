d0480fc2039f83d39348d61478460866c4c6ced5
Lirae Que Data <liraedata59@gmail.com>
2024-06-04 14:30:24 +0800

    Add validate method in login_serializer.py

--- api/v1/serializers/login_serializer.py
+++ api/v1/serializers/login_serializer.py
@@ -1,4 +1,9 @@
+from django.contrib.auth.hashers import check_password
 from rest_framework import serializers
+from rest_framework.exceptions import (ParseError,
+                                       NotFound,
+                                       AuthenticationFailed)
+from api.v1.models import User
 
 
 class LoginSerializer(serializers.Serializer):
@@ -7,3 +12,23 @@ class LoginSerializer(serializers.Serializer):
                     required = True)
     password = serializers.CharField(
                     required = True)
+
+    
+    def validate(self, attrs):
+        
+        if not ("email" in attrs and "password" in attrs):
+            raise ParseError(
+                detail = "Invalid request. Please try again.")
+            
+        user = User.objects.get(email = attrs.get("email"))
+        if not user:
+            raise NotFound(
+                detail = "Credential does not exists.")
+        
+        if not check_password(attrs.get("password"), 
+                              user.password):
+            raise AuthenticationFailed(
+                    detail = "Invalid credentials. Please try again.")
+        
+        attrs["user"] = user
+        return attrs
