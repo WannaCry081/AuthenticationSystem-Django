b4b67ab923c5bc4a00e7cb77a74a4532bf2fb9f7
Lirae Que Data <liraedata59@gmail.com>
2024-06-04 22:01:34 +0800

    Update recover create method in recover_viewset.py

--- api/v1/viewsets/recover_viewset.py
+++ api/v1/viewsets/recover_viewset.py
@@ -1,5 +1,9 @@
-from rest_framework import viewsets, mixins
+from django.conf import settings
+from django.core.mail import send_mail
+from rest_framework import viewsets, mixins, status
 from rest_framework.response import Response
+from rest_framework.exceptions import (ParseError,
+                                       AuthenticationFailed)
 from api.v1.serializers import (ResetPasswordSerializer,
                                 ForgotPasswordSerializer)
 
@@ -12,4 +16,34 @@ class RecoverViewSet(viewsets.GenericViewSet,
 
 
     def create(self, request, *args, **kwargs): 
-        return super().create(request, *args, **kwargs)
+        try:
+            serializer = self.get_serializer(data = request.data)
+            serializer.is_valid(raise_exception = True)
+            
+            user = serializer.validated_data.get("user")
+            
+            send_mail(
+                "Reset Password",
+                f"Code: {user.reset_code}",
+                settings.EMAIL_HOST_USER,
+                [user.email],
+                fail_silently = True,
+            )
+            return Response({
+                "detail" : f"Successfully send code to {request.data['email']}."
+            }, status = status.HTTP_200_OK)
+
+        except ParseError as e:
+            return Response({
+                "detail" : str(e)
+            }, status = status.HTTP_400_BAD_REQUEST)
+        
+        except AuthenticationFailed as e:
+            return Response({
+                "detail" : str(e)
+            }, status = status.HTTP_403_FORBIDDEN)
+        
+        except:
+            return Response({
+                "detail" : "Internal Server Error"
+            }, status = status.HTTP_500_INTERNAL_SERVER_ERROR )
