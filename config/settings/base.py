76dac2e1e5d7f60003c90de1b7dc751f8ced8240
Lirae Que Data <liraedata59@gmail.com>
2024-06-04 22:30:12 +0800

    Update times in SIMPLE_JWT

--- config/settings/base.py
+++ config/settings/base.py
@@ -133,8 +133,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days = 1),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days = 30),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes = 30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(hours = 1),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
     "UPDATE_LAST_LOGIN": False,
