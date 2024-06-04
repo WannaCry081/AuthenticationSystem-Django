7164f218964192541499df7184352b0474bcbd89
Lirae Que Data <liraedata59@gmail.com>
2024-06-04 14:11:42 +0800

    Add validate method in register_serializer.py

--- api/v1/serializers/register_serializer.py
+++ api/v1/serializers/register_serializer.py
@@ -1,4 +1,7 @@
+from bleach import clean
 from rest_framework import serializers
+from rest_framework.exceptions import (ParseError,
+                                       AuthenticationFailed)
 from api.v1.models import User
 
 
@@ -25,8 +28,30 @@ class RegisterSerializer(serializers.ModelSerializer):
                   "firstName",
                   "lastName",
                   "email",
-                  "password"
+                  "password",
                   "rePassword"]
         extra_kwargs = {
             "id" : { "read_only" : True }
         }
+
+    
+    def validate(self, attrs):
+        
+        fields = ["username", "first_name", "last_name",
+                  "email", "password", "rePassword"]
+        
+        if not all(field in attrs for field in fields):
+            raise ParseError(
+                    detail = "Invalid request. Please try again.")
+        
+        if attrs["password"] != attrs["rePassword"]:
+            raise AuthenticationFailed(
+                detail = "Credentials does not match. Please try again.")
+        
+        attrs["username"] = clean(attrs.get("username"))
+        attrs["first_name"] = clean(attrs.get("first_name"))
+        attrs["last_name"] = clean(attrs.get("last_name"))
+        attrs["password"] = clean(attrs.get("password"))
+        
+        attrs.pop("rePassword")
+        return attrs
