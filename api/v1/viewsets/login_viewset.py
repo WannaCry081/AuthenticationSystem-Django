b030e516fd0165d7d5381b34f11f3bdf4b5b9b28
Lirae Que Data <liraedata59@gmail.com>
2024-06-05 01:02:33 +0800

    Add documentation in login_viewset.py

--- api/v1/viewsets/login_viewset.py
+++ api/v1/viewsets/login_viewset.py
@@ -9,13 +9,37 @@ from api.v1.serializers import LoginSerializer
 
 class LoginViewSet(viewsets.GenericViewSet, 
                    mixins.CreateModelMixin):
+    """
+    A viewset for logging in a user. This viewset returns access and refresh tokens for authenticated users.
+    """
 
 
     serializer_class = LoginSerializer
     throttle_classes = [AnonRateThrottle]
 
 
+    @swagger_auto_schema(
+        operation_summary = "Logs in a user.",
+        operation_description = "This endpoint returns the access and refresh token for authenticated users.",
+        responses = {
+            status.HTTP_200_OK: openapi.Response("Ok"),
+            status.HTTP_400_BAD_REQUEST: openapi.Response("Bad Request"),
+            status.HTTP_403_FORBIDDEN: openapi.Response("Forbidden"),
+            status.HTTP_500_INTERNAL_SERVER_ERROR: openapi.Response("Internal Server Error")
+        }
+    )
     def create(self, request, *args, **kwargs):
+        """
+        This endpoint logs in a user and returns the JWT Token.
+
+        Args:
+            request: The HTTP request containing the user credentials.
+            *args: Additional arguments.
+            **kwargs: Additional keyword arguments.
+
+        Returns:
+            Response: A Response object containing the JWT tokens or an error message.
+        """
         try:
             serializer = self.get_serializer(data = request.data)
             serializer.is_valid(raise_exception = True)
