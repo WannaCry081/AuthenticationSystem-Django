c32edbba1268ab8bcc15e439347bbb18d92a0c38
Lirae Que Data <liraedata59@gmail.com>
2024-06-04 22:14:55 +0800

    Add verify method in recover_viewset.py

--- api/v1/viewsets/recover_viewset.py
+++ api/v1/viewsets/recover_viewset.py
@@ -1,9 +1,11 @@
 from django.conf import settings
 from django.core.mail import send_mail
 from rest_framework import viewsets, mixins, status
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.exceptions import (ParseError,
-                                       AuthenticationFailed)
+                                       AuthenticationFailed,
+                                       NotFound)
 from api.v1.serializers import (ResetPasswordSerializer,
                                 ForgotPasswordSerializer)
 
@@ -54,3 +56,34 @@ class RecoverViewSet(viewsets.GenericViewSet,
             return Response({
                 "detail" : "Internal Server Error"
             }, status = status.HTTP_500_INTERNAL_SERVER_ERROR )
+
+
+    @action(methods = ["POST"], detail = False)
+    def verify(self, request, pk = None):
+        try:
+            serializer = self.get_serializer(data = request.data)
+            serializer.is_valid(raise_exception = True)
+            
+            return Response({
+                "detail" : "Password successfully updated."
+            }, status = status.HTTP_200_OK)
+
+        except ParseError as e:
+            return Response({
+                "detail" : str(e)
+            }, status = status.HTTP_400_BAD_REQUEST)
+        
+        except AuthenticationFailed as e:
+            return Response({
+                "detail" : str(e)
+            }, status = status.HTTP_403_FORBIDDEN)
+        
+        except NotFound as e:
+            return Response({
+                "detail" : str(e)
+            }, status = status.HTTP_404_NOT_FOUND)
+
+        except:
+            return Response({
+                "detail" : "Internal Server Error"
+            }, status = status.HTTP_500_INTERNAL_SERVER_ERROR)
