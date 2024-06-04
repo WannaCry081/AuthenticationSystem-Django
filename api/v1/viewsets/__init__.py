00f5b3ea9cbe7b6014eba84b84243cbc8453bcfc
Lirae Que Data <liraedata59@gmail.com>
2024-06-04 12:46:57 +0800

    Add viewsets exports in __init__.py

---
+++ api/v1/viewsets/__init__.py
@@ -0,0 +1,8 @@
+from api.v1.viewsets.login_viewset import LoginViewSet
+from api.v1.viewsets.register_viewset import RegisterViewSet
+from api.v1.viewsets.recover_viewset import RecoverViewSet
+
+
+__all__ = ["LoginViewSet",
+           "RegisterViewSet",
+           "RecoverViewSet"]
