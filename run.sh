b8e6dcc199391e916ca90b6d5bfee46032b18688
Lirae Que Data <liraedata59@gmail.com>
2024-06-04 22:40:35 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/bash
 
+if [ ! -d "$(pwd)/venv" ]; then 
+    virtualenv venv
+    source venv/Scripts/activate
+    pip install -r requirements.txt
+else
+    source venv/Scripts/activate
+fi
+
 python manage.py makemigrations
 python manage.py migrate
 python manage.py runserver
+
+deativate
