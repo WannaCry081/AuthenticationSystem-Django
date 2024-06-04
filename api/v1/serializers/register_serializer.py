bc8a2ec94976ac859f58e626470c50f1e28fd55e
Lirae Que Data <liraedata59@gmail.com>
2024-06-04 14:12:03 +0800

    Add create method in register_serializer.py

--- api/v1/serializers/register_serializer.py
+++ api/v1/serializers/register_serializer.py
@@ -55,3 +55,8 @@ class RegisterSerializer(serializers.ModelSerializer):
         
         attrs.pop("rePassword")
         return attrs
+
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
