ac65114c9c506e2c0394957f32fadd1489936d64
Lirae Que Data <liraedata59@gmail.com>
2024-06-20 23:15:06 +0800

    Update validate method in register_serializer.py

--- api/v1/serializers/register_serializer.py
+++ api/v1/serializers/register_serializer.py
@@ -1,18 +1,11 @@
 from bleach import clean
 from rest_framework import serializers
-from rest_framework.exceptions import (ParseError,
-                                       AuthenticationFailed)
+from rest_framework.exceptions import AuthenticationFailed
 from api.v1.models import User
 
 
 class RegisterSerializer(serializers.ModelSerializer):
 
-    firstName = serializers.CharField(
-                        required = True,
-                        source = "first_name")    
-    lastName = serializers.CharField(
-                        required = True,
-                        source = "last_name")    
     password = serializers.CharField(
                         required = True,
                         write_only = True)
@@ -25,8 +18,6 @@ class RegisterSerializer(serializers.ModelSerializer):
         model = User
         fields = ["id",
                   "username",
-                  "firstName",
-                  "lastName",
                   "email",
                   "password",
                   "rePassword"]
@@ -36,21 +27,13 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     
     def validate(self, attrs):
-        
-        fields = ["username", "first_name", "last_name",
-                  "email", "password", "rePassword"]
-        
-        if not all(field in attrs for field in fields):
-            raise ParseError(
-                    detail = "Invalid request. Please try again.")
-        
+
         if attrs["password"] != attrs["rePassword"]:
             raise AuthenticationFailed(
                 detail = "Credentials does not match. Please try again.")
         
         attrs["username"] = clean(attrs.get("username"))
-        attrs["first_name"] = clean(attrs.get("first_name"))
-        attrs["last_name"] = clean(attrs.get("last_name"))
+        attrs["email"] = clean(attrs.get("email"))
         attrs["password"] = clean(attrs.get("password"))
         
         attrs.pop("rePassword")
