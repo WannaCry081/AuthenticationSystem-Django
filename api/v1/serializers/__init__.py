02b06e096bd0996922205a2c87ff835a55c23c1a
Lirae Que Data <liraedata59@gmail.com>
2024-06-21 20:11:26 +0800

    Add user serializer export

--- api/v1/serializers/__init__.py
+++ api/v1/serializers/__init__.py
@@ -2,9 +2,11 @@ from api.v1.serializers.login_serializer import LoginSerializer
 from api.v1.serializers.register_serializer import RegisterSerializer
 from api.v1.serializers.recover_serializer import (ResetPasswordSerializer,
                                                    ForgotPasswordSerializer)
+from api.v1.serializers.user_serializer import UserSerializer
 
 
 __all__ = ["LoginSerializer",
            "RegisterSerializer",
            "ResetPasswordSerializer",
-           "ForgotPasswordSerializer"]
+           "ForgotPasswordSerializer",
+           "UserSerializer"]
