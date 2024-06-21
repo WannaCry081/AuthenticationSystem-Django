aab93531cd7d3bf347725c713390c2bf9d6f9030
Lirae Que Data <liraedata59@gmail.com>
2024-06-22 01:15:09 +0800

    Add extra_kwargs field in user_serializer.py

--- api/v1/serializers/user_serializer.py
+++ api/v1/serializers/user_serializer.py
@@ -2,15 +2,19 @@ from rest_framework import serializers
 from api.v1.models import User
 
 class UserSerializer(serializers.ModelSerializer):
-  
-  firstName = serializers.CharField(source = "first_name")
-  lastName = serializers.CharField(source = "last_name")
-  
-  class Meta:
     
-    model = User
-    fields = ["id",
-              "username",
-              "email",
-              "firstName",
-              "lastName"]
+    firstName = serializers.CharField(source = "first_name")
+    lastName = serializers.CharField(source = "last_name")
+    
+    class Meta:
+    
+        model = User
+        fields = ["id",
+                  "username",
+                  "email",
+                  "firstName",
+                  "lastName"]
+        extra_kwargs = {
+            "id" : {"read_only" : True},
+            "email" : {"read_only" : True}
+        }
