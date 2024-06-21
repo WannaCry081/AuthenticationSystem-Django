bf6e32c9c346a4b0c3cb6362f74c67e7d9a4353e
Lirae Que Data <liraedata59@gmail.com>
2024-06-21 20:12:31 +0800

    Add new route in urls.py

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -4,16 +4,17 @@ from rest_framework_simplejwt.views import (TokenBlacklistView,
                                             TokenRefreshView)
 from api.v1.viewsets import (LoginViewSet,
                              RegisterViewSet,
-                             RecoverViewSet)
+                             RecoverViewSet,
+                             UserViewSet)
 
 
 auth_route = routers.DefaultRouter()
-
-
 auth_route.register(r"login", LoginViewSet, "login")
 auth_route.register(r"register", RegisterViewSet, "register")
 auth_route.register(r"reset-password", RecoverViewSet, "reset-password")
 
+route = routers.DefaultRouter()
+route.register(r"users", UserViewSet, "users")
 
 urlpatterns = [
     path("auth/", include([
@@ -22,5 +23,7 @@ urlpatterns = [
              name = "token-blacklist"),
         path("refresh/", TokenRefreshView.as_view(), 
              name = "token-refresh")
-    ]))
+    ])),
+    
+    path("", route.urls)
 ]
