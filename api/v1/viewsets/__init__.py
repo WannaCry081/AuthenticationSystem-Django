d65a1d71e631f7b90c3f36b4c2b23050d4fa1ace
Lirae Que Data <liraedata59@gmail.com>
2024-06-21 20:12:11 +0800

    Add user viewset export

--- api/v1/viewsets/__init__.py
+++ api/v1/viewsets/__init__.py
@@ -1,8 +1,10 @@
 from api.v1.viewsets.login_viewset import LoginViewSet
 from api.v1.viewsets.register_viewset import RegisterViewSet
 from api.v1.viewsets.recover_viewset import RecoverViewSet
+from api.v1.viewsets.user_viewset import UserViewSet
 
 
 __all__ = ["LoginViewSet",
            "RegisterViewSet",
-           "RecoverViewSet"]
+           "RecoverViewSet",
+           "UserViewSet"]
