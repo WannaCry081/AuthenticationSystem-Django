6567ee9d09956a949a453bf642747ebcbba93059
Lirae Que Data <liraedata59@gmail.com>
2024-06-22 01:01:26 +0800

    Add user verification in retrieve method

--- api/v1/viewsets/user_viewset.py
+++ api/v1/viewsets/user_viewset.py
@@ -1,4 +1,8 @@
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets, mixins, status
+from rest_framework.response import Response
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 from api.v1.models import User
 from api.v1.serializers import UserSerializer
 
@@ -7,5 +11,20 @@ class UserViewSet(viewsets.GenericViewSet,
                   mixins.UpdateModelMixin, 
                   mixins.DestroyModelMixin):
   
-  queryset = User.objects.all()
-  serializer_class = UserSerializer
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    
+    
+    def check_object_permissions(self, request, obj):
+        if request.user.id != obj.id:
+            raise AuthenticationFailed(
+                detail = "Unauthorized access.")
+
+        return super().check_object_permissions(request, obj)
+
+
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
+     
