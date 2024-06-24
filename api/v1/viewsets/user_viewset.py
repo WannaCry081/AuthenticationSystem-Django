4f9c41fe2b286e844fef383b13badb15595adaeb
Lirae Que Data <liraedata59@gmail.com>
2024-06-25 02:52:29 +0800

    Add me method in user_viewset.py

--- api/v1/viewsets/user_viewset.py
+++ api/v1/viewsets/user_viewset.py
@@ -1,10 +1,12 @@
 from rest_framework import viewsets, mixins, status
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from api.v1.models import User
 from api.v1.serializers import UserSerializer
+
 
 class UserViewSet(viewsets.GenericViewSet, 
                   mixins.RetrieveModelMixin,
@@ -23,11 +25,11 @@ class UserViewSet(viewsets.GenericViewSet,
                 detail = "Unauthorized access.")
 
         return super().check_object_permissions(request, obj)
-
-
+    
+    
     def retrieve(self, request, *args, **kwargs):
         return super().retrieve(request, *args, **kwargs)
-     
+    
     
     def update(self, request, *args, **kwargs):
         return super().update(request, *args, **kwargs)
@@ -35,3 +37,15 @@ class UserViewSet(viewsets.GenericViewSet,
 
     def delete(self, request, *args, **kwargs):
         return super().destroy(request, *args, **kwargs)
+
+
+    @action(methods = ["GET"], detail = False) 
+    def me(self, request, id = None):
+        try:
+            data = UserSerializer(request.user).data
+            return Response(data, status = status.HTTP_200_OK)
+        
+        except:
+            return Response({
+                "detail": "Internal Server Error"
+            }, status = status.HTTP_500_INTERNAL_SERVER_ERROR)
